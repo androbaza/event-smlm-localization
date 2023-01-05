c1e30e39ae89597e801616c5cdf1b6f1d8028782
androbaza <androbaza@tuta.io>
2023-01-05 13:28:36 +0100

    create tensor of rois around peaks

--- event_smlm_localization.py
+++ event_smlm_localization.py
@@ -5,7 +5,7 @@ filepath = '/home/smlm-workstation/event-smlm/Evb-SMLM/generated_frames/tubulin3
 G_s1 = 2.5  # Sigma of the 1st Gaussian Filter
 G_s2 = 6  # Sigma of the 2nd Gaussian Filter
 local_max_scale = 7 # amount w/ the std of difference of gaussians is scaled
-ROI_rad = 3
+roi_rad = 4
 
 im = io.imread(filepath)
 frame = im[150,:,:]
@@ -20,8 +20,11 @@ min_val_LM = np.std(doG) * 7
 peaks = peak_local_max(doG, threshold_abs = min_val_LM)
 
 # remove peaks where they overlap with image borders
-peaks = np.array([peak for peak in peaks if \
-    not peak[0]+ROI_rad >=frame.shape[1] \
-    and not peak[0]-ROI_rad <= 0 \
-    and not peak[1]+ROI_rad >=frame.shape[2] \
-    and not peak[1]-ROI_rad <= 0])
+peaks = torch.tensor([peak for peak in peaks if \
+    not peak[0]+roi_rad >=frame.shape[1] \
+    and not peak[0]-roi_rad <= 0 \
+    and not peak[1]+roi_rad >=frame.shape[2] \
+    and not peak[1]-roi_rad <= 0])
+
+# create a list of tensors from areas around peaks
+roi = torch.tensor([im[peak[0]-roi_rad:peak[0]+roi_rad, peak[1]-roi_rad:peak[1]+roi_rad] for peak in peaks])
--- helpers.py
+++ helpers.py
@@ -4,3 +4,4 @@ from skimage import io
 from scipy.ndimage import gaussian_filter
 from skimage.feature.peak import peak_local_max
 import os
+import torch
